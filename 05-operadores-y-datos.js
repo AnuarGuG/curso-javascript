6c0c4986a6a8c99e0b7c3508f1676d96993525f9
AnuarGuG <anuar.ggu@gmail.com>
2022-11-23 00:40:52 -0600

    me salte al ejercicio 1

--- 05-operadores-y-datos.js
+++ 05-operadores-y-datos.js
@@ -14,9 +14,11 @@ alert("Es resultado de la operacion es: "+operacion);
     var cadena_texto = "Hola vamos a quemar";
     var verdadero_o_falso = true //false //Es un boolean 
     
-    var numero_falso = "33";
-         Number(numero_falso);
+    var numero_falso = "33";  
+         
 
+    console.log(Number(numero_falso)+7);
+  //console.log(parseInt(numero_falso)+7);
     
 
 
