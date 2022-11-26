b4f8fa70faba5fd7a4ecd7c09653246235626410
AnuarGuG <anuar.ggu@gmail.com>
2022-11-26 16:33:49 -0600

    #Ejercicios condicionales con operadores

---
+++ 11-ejercicio.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/*
+    Programa que pida dos numeros y que nos diga cual
+    es el mayor, el menor y si son iguales
+*/
+
+var numero1 = prompt("Introduce el primer numero",0 );
+var numero2 = prompt("Introduce el segundo numero",0);
+
+if (numero1 > numero2) {
+    console.log(numero1+" es mayor que "+numero2));
+} else {
+    console.log
+}
+
