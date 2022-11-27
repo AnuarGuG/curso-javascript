45bd12d122dd71550cf5263367880e10ee4f3eb4
AnuarGuG <anuar.ggu@gmail.com>
2022-11-26 18:41:10 -0600

    #se hizo un bucle-contador,condicioyaumentador

---
+++ 08-buclefor.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//bucle FOR
+//Es una estructura de control que se repite varias veces
+
+/*var numeros = 100;
+    //este ejemplo solo cuenta hasta 99
+for (let i = 0; i < numeros; i++) {
+    
+    console.log("vamos por el numero " +i);
+}*/
+
+var numeros = 100;
+        //este ejemplo cuenta hasta 100
+for (let i = 0; i <= numeros; i++) {
+    
+    console.log("vamos por el numero " +i);
+}
